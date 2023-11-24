523970598665ed43d8e52f949767736943dc1a73
Ahmed Ayman <ahmed.ayman.21021999@gmail.com>
2023-11-24 11:36:13 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="app">
+      الفونت العربي الفونت العربي الفونت العربي
+      <div className="add">Add</div>
+      <div className="no">No</div>
+      App
+    </div>
+  );
+};
+
+export default App;
