bf21c298f7fd7870eb76dd4eb70420fff39d74fa
Ahmed Ayman <ahmed.ayman.21021999@gmail.com>
2023-11-26 16:26:22 +0200

    add emoji and Document title

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
 
-import "./index.css";
-
 import App from "./App.jsx";
 import i18n from "./i18n.js";
 import { DirectionProvider } from "./context/DirectionContext.jsx";
